8bc9673f15fb9fd7832b4602144f1720515027d6
Kalle Kula <kalle@kula.se>
2016-09-09 17:59:12 +0200

    Add a new testmethod for TruncateLeft

--- src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
+++ src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
@@ -3,35 +3,45 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Nardax.Tests
 {
-    [TestClass]
-    public class TruncateStringExtensionsTests
+  [TestClass]
+  public class TruncateStringExtensionsTests
+  {
+    private string _value;
+
+    [TestInitialize]
+    public void TestInitialize()
     {
-        private string _value;
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            _value = "123456789";
-        }
-
-        [TestMethod]
-        public void TruncateLeft_ValueIsLarger_TruncateRemovesLeftSide()
-        {
-            var expected = "9";
-
-            var result = _value.TruncateLeft(1);
-            
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateLeft_ValueIsSmaller_TruncateRemovesLeftSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateLeft(20);
-
-            Assert.AreEqual(expected, result);
-        }
+      _value = "123456789";
     }
+
+    [TestMethod]
+    public void TruncateLeft_ValueIsLarger_TruncateRemovesLeftSide()
+    {
+      var expected = "9";
+
+      var result = _value.TruncateLeft(1);
+
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void TruncateLeft_ValueIsLarger_TruncateRemovesLeftSide_KalleKulaTestar()
+    {
+      var expected = "789";
+
+      var result = _value.TruncateLeft(3);
+
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void TruncateLeft_ValueIsSmaller_TruncateRemovesLeftSide()
+    {
+      var expected = "123456789";
+
+      var result = _value.TruncateLeft(20);
+
+      Assert.AreEqual(expected, result);
+    }
+  }
 }
