2991d511cfe9c550a94ad72a450f891a41cdb97f
Gustav Nyberg <gustav.gn.nyberg@gmail.com>
2016-09-09 17:23:35 +0200

    Added test methods for Queue

---
+++ src/Nardax.UnitTests/QueueExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nardax.UnitTests
+{
+  using System.Collections.Generic;
+
+  [TestClass]
+  public class QueueExtensionsTest
+  {
+    private Queue<string> _queue;
+
+    public QueueExtensionsTest()
+    {
+      _queue = new Queue<string>();
+      _queue.Enqueue("aa");
+      _queue.Enqueue("bb");
+      _queue.Enqueue("cc");
+    }
+
+    [TestMethod]
+    public void EnqueueTest()
+    {
+      _queue.EnqueueRange(new [] {"dd", "ee"});
+
+      Assert.AreEqual("aa", _queue.Dequeue());
+      Assert.AreEqual("bb", _queue.Dequeue());
+      Assert.AreEqual("cc", _queue.Dequeue());
+      Assert.AreEqual("dd", _queue.Dequeue());
+      Assert.AreEqual("ee", _queue.Dequeue());
+    }
+  }
+}
