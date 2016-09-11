3dd37b4879207511ebdd21764ae1792a3b099b4c
Stefan Löfven <stefan@lofven.se>
2016-09-11 14:46:51 +0200

    Fixed missmatched backets after mege and fixed indenting in the file.

--- src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
+++ src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
@@ -3,78 +3,76 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Nardax.Tests
 {
-    [TestClass]
-    public class TruncateStringExtensionsTests
+  [TestClass]
+  public class TruncateStringExtensionsTests
+  {
+    private string _value;
+
+    [TestInitialize]
+    public void TestInitialize()
     {
-        private string _value;
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            _value = "123456789";
-        }
-
-        [TestMethod]
-        public void TruncateLeft_ValueIsLarger_TruncateRemovesLeftSide()
-        {
-            var expected = "9";
-
-            var result = _value.TruncateLeft(1);
-            
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateLeft_ValueIsSmaller_TruncateRemovesLeftSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateLeft(20);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsLarger_TruncateRemovesRightSide()
-        {
-            var expected = "1";
-
-            var result = _value.TruncateRight(1);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsSmaller_TruncateRemovesRightSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateRight(20);
-
-            Assert.AreEqual(expected, result);
-        }
+      _value = "123456789";
     }
-}
 
-      [TestMethod]
-      public void RemoveWhiteChars()
-      {
-        var testString = "i\twill \tsurvive\t";
+    [TestMethod]
+    public void TruncateLeft_ValueIsLarger_TruncateRemovesLeftSide()
+    {
+      var expected = "9";
 
-        var result = testString.RemoveWhiteChars();
+      var result = _value.TruncateLeft(1);
 
-        var expected = "iwillsurvive";
-
-        Assert.AreEqual(expected, result);
-      }
-
-      [TestMethod]
-      public void RepeatMe()
-      {
-        var expected = "käbbelkäbbelkäbbel";
-        var result = "käbbel".RepeatMe(3);
-
-        Assert.AreEqual(expected, result);
-      }
+      Assert.AreEqual(expected, result);
     }
+
+    [TestMethod]
+    public void TruncateLeft_ValueIsSmaller_TruncateRemovesLeftSide()
+    {
+      var expected = "123456789";
+
+      var result = _value.TruncateLeft(20);
+
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void TruncateRight_ValueIsLarger_TruncateRemovesRightSide()
+    {
+      var expected = "1";
+
+      var result = _value.TruncateRight(1);
+
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void TruncateRight_ValueIsSmaller_TruncateRemovesRightSide()
+    {
+      var expected = "123456789";
+
+      var result = _value.TruncateRight(20);
+
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void RemoveWhiteChars()
+    {
+      var testString = "i\twill \tsurvive\t";
+
+      var result = testString.RemoveWhiteChars();
+
+      var expected = "iwillsurvive";
+
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void RepeatMe()
+    {
+      var expected = "käbbelkäbbelkäbbel";
+      var result = "käbbel".RepeatMe(3);
+
+      Assert.AreEqual(expected, result);
+    }
+  }
 }
